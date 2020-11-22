5d447935027f8bb3a6cfef2f2acd6625d4149742
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2020-11-22 17:07:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,21 +19,18 @@ $(function(){
             $.each($(image_src), function () {
                 image_srcNotWebp = $(this).data('notwebp');
                 $(this).css('background-image', 'url("' + image_srcNotWebp + '")');
-
             });
-
         });
-
+    
     $('.box').hover(function(e) {
-        let heightBox = $(this).height() / 2, widthBox = $(this).width() / 2;
-        $('.box').on('mousemove', function(e) {
-            $(this).css("transform", "rotateX(" + -(e.offsetY - heightBox) / 20 + "deg) rotateY(" + (e.offsetX - widthBox) / 20 + "deg) translate3d(0, 0, 45px)")
-
-        });
-        
+        if($(window).width() > 769) {
+           let heightBox = $(this).height() / 2, widthBox = $(this).width() / 2;
+            $('.box').on('mousemove', function(e) {
+                $(this).css("transform", "rotateX(" + -(e.offsetY - heightBox) / 20 + "deg) rotateY(" + (e.offsetX - widthBox) / 20 + "deg) translate3d(0, 0, 45px)")
+            }); 
+        }
     }, 
     function() {
         $(this).css('transform', 'rotateX(0deg) rotateY(0deg)');
     })
-
 });
